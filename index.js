af738cbe2850dec98e6252e528eb21eacf229dd8
Robert Hesselmann <hesselmann.robert@gmail.com>
2022-10-26 11:12:27 +0200

    Add Sorted Queue to fix network delay issue

--- index.js
+++ index.js
@@ -15,23 +15,49 @@ const requestNitradoAPI = async (gameServerId, token) => {
     return data.data;
 }
 
-
+function queueWorker(queue) {
+    queue = queue.sort((a, b) => a.timestamp < b.timestamp)
+    //console.log(queue)
+    for(let i=0; i < queue.length; i++) {
+        if(queue[i] === undefined) continue;
+        console.log(queue[i].msg)
+        delete queue[i]
+    }
+}
 
 (async () => {
 
+    var queue = [];
+
     //console.log(process.env)
+
+    console.log("Loading API")
 
     const data = await requestNitradoAPI(GAMESERVER_ID, TOKEN);
 
     if (data.status === "success") {
-        console.log(JSON.stringify(data))
+        //console.log(JSON.stringify(data))
         const ws = new WebSocket(`wss://${data.data.gameserver.hostsystems.linux.servername}.gamedata.io:34882/docker?token=${TOKEN}&backbuffer=200&service=${GAMESERVER_ID}`)
         //console.log("test")
+        console.log("Connected.")
+
+
+
         ws.on("message", (data) => {
             const json = JSON.parse(data);
-            console.log(json.msg)
+            if (json.type === "stdout")
+                queue.push(json)
+
+            //console.log(json.msg)
         });
+
+        setTimeout(() => {
+            setInterval(queueWorker, 0, queue);
+        },3000)
+
         process.stdin.pipe(require('split')()).on('data', (line) => ws.send(JSON.stringify({"command":line,"container":`${data.data.gameserver.username}-game`})))
+
+
 
     } else {
         console.log(data)
